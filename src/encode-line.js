4f9aef12c8474ac2c79659ba7b158ea882fe3dab
AlexeyTitikov <alexey.titkov.work@gmail.com>
2023-04-17 15:37:23 +0300

    add some solutions

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  // Создаем переменные для закодированной строки и количества повторяющихся символов
+  let encoded = '';
+  let count = 1;
+
+  // Проходим циклом по каждому символу строки
+  for (let i = 0; i < str.length; i++) {
+    // Если текущий символ равен следующему, увеличиваем счетчик
+    if (str[i] === str[i + 1]) {
+      count++;
+    } else {
+      // Если символ не повторяется, добавляем закодированный символ в строку encoded
+      // Если символ повторяется более одного раза,
+      // добавляем количество повторений и символ в закодированную строку
+      encoded += count > 1 ? count + str[i] : str[i];
+      // Сбрасываем счетчик
+      count = 1;
+    }
+  }
+
+  // Возвращаем закодированную строку
+  return encoded;
 }
 
 module.exports = {
