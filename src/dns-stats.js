1ebc9e97385899cc8a51c3551cbd2215bb2ae74f
AlexeyTitikov <alexey.titkov.work@gmail.com>
2023-04-17 14:04:27 +0300

    add some solutions

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  // Создаем пустой объект, который будем использовать для хранения статистики
+  const dnsStats = {};
+
+  // Проходимся по каждому доменному имени в массиве domains
+  domains.forEach(domain => {
+    // Разбиваем каждое доменное имя на части, используя точку как разделитель
+    // и переворачиваем полученный массив частей
+    const parts = domain.split('.').reverse();
+
+    // Создаем пустую переменную dns для хранения DNS-имени
+    let dns = '';
+
+    // Проходимся по каждой части доменного имени
+    parts.forEach(part => {
+      // Добавляем точку и текущую часть доменного имени к переменной dns
+      dns += `.${part}`;
+
+      // Проверяем, есть ли уже такое DNS-имя в объекте dnsStats
+      if (dns in dnsStats) {
+        // Если есть, увеличиваем значение этого ключа на 1
+        dnsStats[dns]++;
+      } else {
+        // Если нет, создаем новый ключ со значением 1
+        dnsStats[dns] = 1;
+      }
+    });
+  });
+
+  // Возвращаем полученную статистику
+  return dnsStats;
 }
 
 module.exports = {
