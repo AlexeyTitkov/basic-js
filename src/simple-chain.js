9f9232654cbc2bd628571f4b1ea6d4f12df2c6f0
AlexeyTitikov <alexey.titkov.work@gmail.com>
2023-04-17 20:51:51 +0300

    add some solutions

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  chain: [], // Массив для хранения элементов цепи
+
+  getLength() { // Метод для получения длины цепи
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  // Метод для добавления нового элемента в цепь
+  addLink(value = '') { // Метод для добавления нового элемента в цепь
+    this.chain.push(`( ${value} )`);
+    return this; // возвращаем объект chainMaker для возможности цепочного вызова методов
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  // Метод для удаления элемента по указанной позиции
+  removeLink(position) {
+    if (!Number.isInteger(position) || position < 1 || position > this.chain.length) {
+      this.chain = []; // очищаем массив, если позиция не является целым числом или выходит за границы массива
+      throw new Error("You can't remove incorrect link!");// Возврщаем ошибку
+    }
+    this.chain.splice(position - 1, 1);// удаляем элемент из массива
+    return this; // возвращаем объект chainMaker для возможности цепочного вызова методов
   },
+
+  // Метод для изменения порядка элементов цепи на обратный
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse(); // изменяем порядок элементов в массиве
+    return this; // возвращаем объект chainMaker для возможности цепочного вызова методов
   },
+
+  // Метод для завершения цепи и возвращения результата в виде строки
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const result = this.chain.join('~~'); // объединяем элементы массива в строку с разделителем ''
+    this.chain = []; // очищаем массив
+    return result; // возвращаем строку-результат
   }
 };
 
